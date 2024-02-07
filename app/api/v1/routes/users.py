10f1b64713d4ea619a8808b83c27da0743a63c43
midnightgb <midnight3424@gmail.com>
2024-02-07 11:26:20 -0500

    Add user creation endpoint and include user routes in main.py

--- app/api/v1/routes/users.py
+++ app/api/v1/routes/users.py
@@ -3,10 +3,11 @@ from sqlalchemy.orm import Session
 #Core dependencies
 from core.database import get_database, server_status
 from core.logger import Logger
+from core.utils import handle_server_down
 #API dependencies
-from app.core.utils import handle_server_down
-from api.v1.schemas.users import UserRead
+from api.v1.schemas.users import UserRead, UserCreate
 from api.v1.models.User import User
+from api.v1.crud.users import create_new_user, get_user_by_email
 
 
 router = APIRouter(
@@ -14,6 +15,16 @@ router = APIRouter(
     tags=["Users"],
     responses={404: {"description": "Not found"}},
 )
+@router.post("/users/create/", response_model=UserRead)
+async def create_user(user: UserCreate, db: Session = Depends(get_database)):
+    if not server_status(db):
+        return handle_server_down()
+    verify_user = get_user_by_email(user.mail, db)
+    if verify_user is None:
+        return create_new_user(user, db)
+    else:
+        raise HTTPException(status_code=400, detail="El email proporcionado ya está en uso.")
+
 
 @router.get("/users/{user_id}", response_model=UserRead)
 def read_user(user_id: int, db: Session = Depends(get_database)):
--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from core.logger import Logger
-from api.v1.routes import test_connection
+from api.v1.routes import test_connection, users
 
 
 Logger.info("Starting API")
@@ -14,3 +14,4 @@ def root():
 
 
 app.include_router(test_connection.router, tags=["Test Connection"])
+app.include_router(users.router, tags=["Users"])
